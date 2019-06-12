0a2a9db2f5a44dbd2a94a97bc1f43c696bdbcb84
Asher Goldberg <asher.goldberg@prs-invivo.com>
2019-06-12 10:55:55 -0400

    new docker

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const app = express()
 //get shoefits router from routes folder
 const sheofitsroutes = require('./routes/shoefits')
 
+console.log(process.env.DATABASE_URL)
+
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
